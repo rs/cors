08c0a294f6133ce99ad6b24e3b36ffd2a7e43529
jub0bs <jcretel-infosec+github@protonmail.com>
2024-04-28 11:09:54 +0200

    Use New to build corswrapper in tests

--- wrapper/gin/gin_test.go
+++ wrapper/gin/gin_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
-	"github.com/rs/cors"
 )
 
 func init() {
@@ -43,9 +42,7 @@ func TestCorsWrapper_buildAbortsWhenPreflight(t *testing.T) {
 	ctx.Status(http.StatusAccepted)
 	res.Code = http.StatusAccepted
 
-	handler := corsWrapper{Cors: cors.New(Options{
-		// Intentionally left blank.
-	})}.build()
+	handler := New(Options{ /* Intentionally left blank. */ })
 
 	handler(ctx)
 
@@ -54,23 +51,5 @@ func TestCorsWrapper_buildAbortsWhenPreflight(t *testing.T) {
 	}
 	if res.Code != http.StatusOK {
 		t.Error("Should abort with 200 OK status")
-	}
-}
-
-func TestCorsWrapper_buildNotAbortsWhenPassthrough(t *testing.T) {
-	res := httptest.NewRecorder()
-	ctx, _ := gin.CreateTestContext(res)
-	ctx.Request, _ = http.NewRequest("OPTIONS", "http://example.com/foo", nil)
-	ctx.Request.Header.Add("Origin", "http://example.org")
-	ctx.Request.Header.Add("Access-Control-Request-Method", "POST")
-
-	handler := corsWrapper{cors.New(Options{
-		OptionsPassthrough: true,
-	}), true}.build()
-
-	handler(ctx)
-
-	if ctx.IsAborted() {
-		t.Error("Should not abort when OPTIONS passthrough enabled")
 	}
 }
